b57de6ec331b7d465ffa096914cef70602f1b4bc
UserP.Desktop <al221811777@gmail.com>
2019-08-13 02:02:30 -0500

    finalizado

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -17,14 +17,16 @@ int main(){
     b = -(3*distancia)*I1;
     c = (3*pow(distancia,2))*I1;
     d = -(pow(distancia,3))*I1;
-    cout<<"\nEcuacion: I'(x) = "<<a<<"(x^3)"<<b<<"(x^2)+"<<c<<"x"<<d<<"\n";
-
+    cout<<"\nI'(x) = "<<a<<"(x^3)"<<b<<"(x^2)+"<<c<<"x"<<d<<"\n";
     z =(double)1/3;
     e = -(pow(27,2))+(3*(a*c));
     f = (-2*(pow(b,3)))+(9*(a*b*c))-27*(pow(a,2)*d);
     g = pow(f+(sqrt(4*pow(e,3)+pow(f,2))),z);
     x1 = (-b/(3.0*a))-((pow(2,z)*e)/(3*a*g))+(g/(3*pow(2,z)*a));
     cout<< "La distancia a la fuente de mayor intensidad debe ser = "<<x1<<"\n";
+    cout<<"I''(x) = "<<a*3<<"(x^2)"<<b*2<<"(x)+"<<c<<"\n";
+    cout<<"I''("<<x1<<") ="<<a*3*pow(x1,2)+b*2*x1+c<<".\n";
+    cout<<"Como I''("<<x1<<") > 0 se trata de un minimo.\n";
 system("pause");
 }
 
